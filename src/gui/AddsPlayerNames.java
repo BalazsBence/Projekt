1b23187b4740890e7caf69898eb5e9c8bbbf1fd5
BenceBalazs <bbbence999@gmail.com>
2021-11-13 22:34:38 +0100

    Countinued GUI

--- src/gui/AddsPlayerNames.java
+++ src/gui/AddsPlayerNames.java
@@ -1,5 +1,13 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AddsPlayerNames extends javax.swing.JFrame {
-    
+
+    @FXML
+    void SavePlayerName(ActionEvent event) {
+
+    }
+
 }
